6b6019bf859eaadfc61e0c10fa07e25074855850
Olwethu Ngcobo <nolwethu70@gmail.com>
2025-06-10 15:35:28 +0200

    Implement delete_task command and feature

--- src/main.py
+++ src/main.py
@@ -9,8 +9,14 @@ subparsers = parser.add_subparsers(dest='commands')
 
 add_parser = subparsers.add_parser('add', help='adding a task')
 add_parser.add_argument('description', help='description of the task')
+
+delete_parser = subparsers.add_parser('delete', help='deleting a task')
+delete_parser.add_argument('id',type=int, help='id of task')
 args = parser.parse_args()
 
 if args.commands == 'add':
     task_manager.path_exists()
     task_manager.add_task(args.description)
+elif args.commands == 'delete':
+    task_manager.path_exists()
+    task_manager.delete_task(args.id)
--- src/task_manager.py
+++ src/task_manager.py
@@ -21,29 +21,58 @@ class TaskManager():
     def add_task(self, description):
         """Adds a task to the json file"""
 
-        if self.path_exists:
+        try:
+            if self.path_exists:
 
-            with open(self.path, 'r') as f:
+                with open(self.path, encoding='utf-8', mode='r') as f:
+                    tasks = json.load(f)
+                    current_date = date.today().strftime("%d/%m/%Y")
+
+                id = max({task['id'] for task in tasks}, default=0) + 1
+                createdAt = current_date
+                updatedAt = current_date
+
+                new_task = Task(id, description, createdAt, updatedAt, status='todo')
+                task = {
+                    'id': new_task.id,
+                    'description': new_task.description,
+                    'status': new_task.status,
+                    'createdAt': new_task.createdAt,
+                    'updatedAt': new_task.updatedAt
+                }
+
+                tasks.append(task)
+                with open(self.path, encoding='utf-8', mode='w') as f:
+                    json.dump(tasks, f, indent=2)
+            
+                print(f"Task added successfully (ID: {id})")
+            
+                return tasks
+        except:
+            print("Something went wrong")
+    
+    def delete_task(self, id):
+        """Deletes a task from json file"""
+
+        try:
+            with open('tasks.json', encoding='utf-8', mode='r') as f:
                 tasks = json.load(f)
-                current_date = date.today().strftime("%d/%m/%Y")
-
-            id = max({task['id'] for task in tasks}, default=0) + 1
-            createdAt = current_date
-            updatedAt = current_date
-
-            new_task = Task(id, description, createdAt, updatedAt, status='todo')
-            task = {
-                'id': new_task.id,
-                'description': new_task.description,
-                'status': new_task.status,
-                'createdAt': new_task.createdAt,
-                'updatedAt': new_task.updatedAt
-            }
-
-            tasks.append(task)
-            with open(self.path, encoding='utf-8', mode='w') as f:
+            
+            if len(tasks) == 0:
+                print("You haven't created any tasks yet to delete")
+                return
+            
+            for i, task in enumerate(tasks):
+                if task.get('id') == id:
+                    del_task = tasks.pop(i)
+                    break
+            
+            with open('tasks.json', encoding='utf-8', mode='w') as f:
                 json.dump(tasks, f, indent=2)
+            
+            print(f"Task has been deleted (ID: {del_task['id']})")
+
+            return del_task
         
-            print(f"Task added successfully (ID: {id})")
-        
-            return tasks
+        except:
+            print("Something went wrong")
